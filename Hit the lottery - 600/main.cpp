ae6aeb1d24e75d6d0b7b134787cb1f6376d0428f
Eduardo Perez Regin <dardo1818@gmail.com>
2020-04-01 00:52:33 -0600

    Final 1

--- Hit the lottery - 600/main.cpp
+++ Hit the lottery - 600/main.cpp
@@ -1,3 +1,16 @@
+//https://codeforces.com/problemset/problem/996/A
+
+/* This.
+ * Well, my logic was this: We have 5 kinds of bills and n money,
+ * to know how many bills you can get, you need to divide between all the 
+ * bills, if it's different than 0, you have have how many bills you can have.
+ * Once you know how many bills you have, you need to multiply how many bills you
+ * get by the bill, to finally substract your money to that multiplication.
+ *
+ * 43 / 20 = 2, I got two bills.
+ * 20 * 2 = 40, because I have to bills of 20, I have in total 40
+ * 43 - 40 = 3, now I have this left. */
+
 #include <iostream>
 
 int main() {
