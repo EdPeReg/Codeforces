f24f2a0f8014f490384ad98d98e0bbc7753f75e3
Eduardo Perez Regin <dardo1818@gmail.com>
2020-03-31 21:21:13 -0600

    First try

---
+++ Bear and bit brother - 600/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+	int a, b, year = 1;
+	std::cin >> a >> b;
+
+	while(true) {
+		a *= 3;
+		b *= 2;
+		if(a > b) {
+			std::cout << year;
+			break;
+		}
+		year++;
+
+	}
+
+	return 0;
+}
