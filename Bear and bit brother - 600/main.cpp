31303851c248d5e7b6a6de6ddfa673afea5833af
Eduardo Perez Regin <dardo1818@gmail.com>
2020-03-31 23:17:43 -0600

    Not completed

--- Bear and bit brother - 600/main.cpp
+++ Bear and bit brother - 600/main.cpp
@@ -12,7 +12,6 @@ int main() {
 			break;
 		}
 		year++;
-
 	}
 
 	return 0;
---
+++ Hulk - 600/main.cpp
@@ -0,0 +1,34 @@
+//https://codeforces.com/problemset/problem/705/A
+
+/* Incomplete, I don't know how to concatenate the words, I noticed
+ * that when it's even that word "i love" it's added, when it's odd
+ * the word "I hate" it's added. */
+
+#include <iostream>
+#include <string>
+
+int main() {
+	int n;
+	std:: cin >> n;
+
+	std::string that = "that ";
+	std::string iHate = "I hate";
+	std::string iLove = "I love";
+
+	if(n == 1) {
+		std::cout << "I hate";
+	} else {
+		while(n) {
+			if(n % 2  == 0) {
+				std::cout << iLove;
+			} else {
+				std::cout << iHate;
+			}
+			n--;
+		}
+	}
+
+	std::cout << " it";
+
+	return 0;
+}
