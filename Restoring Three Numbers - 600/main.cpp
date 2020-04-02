83e5e5657cf189a2fd65b85eee57db712b781a8d
Eduardo Perez Regin <dardo1818@gmail.com>
2020-04-01 22:32:46 -0600

    Final 1

--- Restoring Three Numbers - 600/main.cpp
+++ Restoring Three Numbers - 600/main.cpp
@@ -1,3 +1,23 @@
+//https://codeforces.com/problemset/problem/1154/A
+
+/* Well, this is a basic problem, let's take the first example:
+ * 3 6 5 4
+ * 2 1 3
+ *
+ * Where a = 2, b = 1, c = 3.
+ * 2 + 1 = 3
+ * 2 + 3 = 5
+ * 1 + 3 = 4
+ * 2 + 1 + 3 = 5
+ *
+ * The sum above, match with the input numbers, but this is the case when you know the output
+ * (which you don't), 
+ * Get the max number of the input and substract it to each element (but not with itself). 
+ * max = 6
+ * 6 - 3 = 3
+ * 6 - 5 = 1
+ * 6 - 4 = 2. */
+
 #include <iostream>
 #include <algorithm>
 
