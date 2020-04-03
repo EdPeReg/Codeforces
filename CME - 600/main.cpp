5451f58cd1e131b4ed8dec80be63ea7bafc87d03
Eduardo Perez Regin <dardo1818@gmail.com>
2020-04-02 21:46:20 -0600

    Not completed

---
+++ CME - 600/main.cpp
@@ -0,0 +1,34 @@
+//https://codeforces.com/problemset/problem/1223/A
+
+/* 
+ * Well, maybe one step is to divide the number by 2.
+ *
+ * Like this:
+ *
+ * n  = 5
+ * 5 / 2 = 2
+ *
+ * Once you divided by 2, you substract that result to n and the
+ * result will be on the right. And the result of the devition on the left.
+ * Now you see if you need to complete or not. In this case you need 1
+ * to match the equation.
+ *
+ * 2 + 1 = 3 
+ *
+ * */
+
+#include <iostream>
+
+int main() {
+	int n;
+	std::cin >> n;
+	
+	while(n--) {
+		int n2;
+		std::cin >> n2;
+
+		n2 /= 2;
+	}
+
+	return 0;
+}
