459e29f86175eee2e4b02dbecfc32d67cae6fe23
Eduardo Perez Regin <dardo1818@gmail.com>
2020-04-03 21:15:31 -0600

    Second try

--- Love 'A' - 600/main.cpp
+++ Love 'A' - 600/main.cpp
@@ -1,15 +1,22 @@
+
+/* A case it's also when there are only a. */
+
 #include <iostream>
 #include <string>
 
 int main() {
 	int total = 0;
+	bool flag = false;
 	std::string str;
 	std::cin >> str;
 
 	for(size_t i(0); i < str.size(); i++) {
 		if(str[i] == 'a') total++;
+		else flag = true;
 	}
-	std::cout << total + 1;
+	
+	if(flag == false) std::cout << total;
+	else std::cout << total + 1;
 
 	return 0;
 }
