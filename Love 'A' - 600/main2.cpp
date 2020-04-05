d0af2fc06957c6baeed4ccb591e8bd170ef1cbfb
Eduardo Perez Regin <dardo1818@gmail.com>
2020-04-05 15:22:53 -0500

    Fouth try

---
+++ Love 'A' - 600/main2.cpp
@@ -0,0 +1,29 @@
+//https://codeforces.com/problemset/problem/1146/A
+
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string str;
+	int noA = 0;
+	int a = 0;
+	std::cin >> str;
+
+	for(size_t i(0); i < str.size(); i++) {
+		if(str[i] != 'a') {
+			noA++;
+		} else {
+			a++;
+		}
+	}
+	
+	if(noA > a) {
+		std::cout << a + 1;
+	} else if(noA == a) {
+		std::cout << str.size() - 1;
+	} else {
+		std::cout << str.size();
+	}
+
+	return 0;
+}
