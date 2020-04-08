cce96b36eaa37fc1525218218fcb202858069e70
Eduardo Perez Regin <dardo1818@gmail.com>
2020-04-08 16:23:26 -0500

    Not completed

---
+++ 700/Presents/main.cpp
@@ -0,0 +1,10 @@
+// https://codeforces.com/problemset/problem/136/A
+
+/* I didn't undestand the problem, maybe I need to use permutations. */
+
+#include <iostream>
+
+int main() {
+
+	return 0;
+}
