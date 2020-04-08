a181b6ec66df9e2a3c6e5d7be3773b1266e50f81
Eduardo Perez Regin <dardo1818@gmail.com>
2020-04-08 15:46:18 -0500

    Final 1

--- 700/Elephant/main.cpp
+++ 700/Elephant/main.cpp
@@ -1,58 +1,21 @@
 // https://codeforces.com/problemset/problem/617/A
 
-/* I'm not proud of it but well, 
- * basically you need to count the number of operations I did in the 
- * substraction, we stat from the highest steps, and after that we go to the 
- * second highest step and so on.
- * 
- * n = 12
- * n = 12 - 5 -> 7
- * n = 7 - 5  -> 2
- * n = 2 - 5  -> Wrong, now start from the second highest step.
- *
- * n = 2 - 4  -> Wrong
- * n = 2 - 3  -> Wrong
- * n = 2 - 2  -> 0, Finish
- *
- * The total valid operations are 3.
+/* 
+ * It's optimal to do the biggest possible step everytime. So elephant should do several steps by distance 5 
+ * and one or zero step by smaller distance. Answer equals to x/5 (rounding upward). 
  *
  * */
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
-	int n, total = 0;
+	double n;
 	cin >> n;
 
-	// These ugly else if should be done in another way.
-	if(n == 5) cout << 1;
-	else if(n == 4) cout << 1;
-	else if(n == 3) cout << 1;
-	else if(n == 2) cout << 1;
-	else if(n == 1) cout << 1;
-	else {
-		for(int i = 5; i >= 1; i--) {
-			// To avoid negative numbers.
-			if(n - i < 0) {
-				continue;
-			} else {
-				n = n - i;
-			}
-
-			while(n >= 0) {
-				total++;
-				// To avoid negative numbers.
-				if(n - i < 0) {
-					break;
-				} else {
-					n = n - i;
-				}
-			} 
-		}
-		cout << total;
-	}
+	cout << ceil(n / 5);
 
 	return 0;
 }
