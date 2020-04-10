34d5493abe04bc5dc1a3efa5941640e18fea23e3
Eduardo Perez Regin <dardo1818@gmail.com>
2020-04-09 22:06:06 -0500

    Final 1

--- 700/Drinks/main.cpp
+++ 700/Drinks/main.cpp
@@ -1,3 +1,12 @@
+// https://codeforces.com/problemset/problem/200/B
+
+/* Basically it's just a rule of three. 
+ * 
+ * 3    100%
+ * 200  x
+ *
+ * */
+
 #include <iostream>
 
 using namespace std;
@@ -6,11 +15,15 @@ int main() {
 	int n, total = 0;
 	cin >> n;
 
+	// To sum the the volume fraction of orange juice.
 	for(int i = 0; i < n; i++) {
 		double drink;
 		cin >> drink;
 		total += drink;
 	}
+	// To have the correct decimal place, we multiply by 100 the n, with this
+	// you can move the decimal point to the left n places, with 100 we move 
+	// it two places to the left.
 	printf("%.12f\n", (double)total * 100 / (n * 100));
 		
 	return 0;
