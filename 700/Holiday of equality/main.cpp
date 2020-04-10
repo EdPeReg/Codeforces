16a0e4f7c461e8abc6e84f286f468f4ecf82a5d6
Eduardo Perez Regin <dardo1818@gmail.com>
2020-04-10 12:07:34 -0500

    Final 1

--- 700/Holiday of equality/main.cpp
+++ 700/Holiday of equality/main.cpp
@@ -1,3 +1,12 @@
+//https://codeforces.com/problemset/problem/758/A
+
+// 1. 19 min.
+
+/* You will need to find the max number of the array, having the max number,
+ * you need to substract that max number for each number in the array and that result
+ * needs to be added.
+ * */
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
