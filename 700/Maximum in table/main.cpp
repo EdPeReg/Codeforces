af78b887827e1426770e5a61858681e8fb3aef90
Eduardo Perez Regin <dardo1818@gmail.com>
2020-04-10 13:35:23 -0500

    Final 2

--- 700/Maximum in table/main.cpp
+++ 700/Maximum in table/main.cpp
@@ -1,20 +1,15 @@
 // https://codeforces.com/problemset/problem/509/A
-// 1. 19 min
-// 2. 21 min
 
-/* I think this is not efficient but well, basically I filled the matrix with 1 at the beggining,
- * and after that, I modify the other values using the formula provided to finally, have the max
- * number of the matrix.
- *
- * Note, it seems that the max number is always the last element in the matrix. */
+/* Another way to do it, if you notice, the last position of the matrix will be ALWAYS the max number,
+ * you only print that position without iterate all over the matrix.
+ * n - 1 because we are starting at position 0. */
 
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
 int main() {
-	int n, max = 0;
+	int n;
 	cin >> n;
 	int matrix[n][n];
 
@@ -30,18 +25,7 @@ int main() {
 		}
 	}
 
-	if(n == 1) {
-		cout << 1 << "\n";
-	} else if(n == 2) {
-		cout << 2 << "\n";
-	} else {
-		for(int i = 1; i < n - 1; i++) {
-			for(int j = 1; j < n - 1; j++) {
-				max = std::max(matrix[i][j], matrix[i + 1][j + 1]);
-			}
-		}
-		cout << max << "\n";
-	}
+	cout << matrix[n - 1][n - 1] << "\n";
 
 	return 0;
 }
