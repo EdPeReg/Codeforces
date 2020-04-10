3737c3fa081cbb289fc616351ddffdf27beff551
Eduardo Perez Regin <dardo1818@gmail.com>
2020-04-09 22:17:17 -0500

    Final 1

--- 700/Drinks/main.cpp
+++ 700/Drinks/main.cpp
@@ -1,30 +1,22 @@
 // https://codeforces.com/problemset/problem/200/B
 
-/* Basically it's just a rule of three. 
- * 
- * 3    100%
- * 200  x
- *
- * */
+/* Another way it's just to compute the average. */
 
 #include <iostream>
 
 using namespace std;
 
 int main() {
-	int n, total = 0;
+	int n;
+	double drink, total = 0.0 ;
 	cin >> n;
 
 	// To sum the the volume fraction of orange juice.
 	for(int i = 0; i < n; i++) {
-		double drink;
 		cin >> drink;
 		total += drink;
 	}
-	// To have the correct decimal place, we multiply by 100 the n, with this
-	// you can move the decimal point to the left n places, with 100 we move 
-	// it two places to the left.
-	printf("%.12f\n", (double)total * 100 / (n * 100));
+	printf("%.12f\n", total / n);
 		
 	return 0;
 }
