e8fa94b06174df99be37d25b04154eca4d3f503b
Eduardo Perez Regin <dardo1818@gmail.com>
2020-04-09 20:54:23 -0500

    Final 1

--- 700/Magnets/main.cpp
+++ 700/Magnets/main.cpp
@@ -1,3 +1,5 @@
+// https://codeforces.com/problemset/problem/344/A
+
 #include <iostream>
 #include <vector>
 
@@ -9,16 +11,20 @@ int main() {
 
 	vector<int> m;
 	for(int i(0); i < n; i++) {
-		int gm;
+		int gm; // Group magnet.
 		cin >> gm;
 		m.push_back(gm);
 	}
 
 	for(int i(0); i < n - 1; i++) {
+		// Check if the next element of the vector is different, if it is, there is
+		// a new magnet group.
 		if(m.at(i) != m.at(i + 1)) {
 			group++;
 		}
 	}
+	// To have the correct magnets group, if you don't add + 1, you will not take in 
+	// consideration the last group.
 	cout << group + 1 << "\n";
 
 
