85cea363198b729125e0a05200cccbca17e7a84e
Eduardo Perez Regin <dardo1818@gmail.com>
2020-04-11 16:32:37 -0500

    Final 2

--- 700/Black square/main.cpp
+++ 700/Black square/main.cpp
@@ -1,14 +1,10 @@
 // https://codeforces.com/problemset/problem/431/A
 // 35 min.
-// NOT COMPLETED.
+// 15 min.
+// 50 min total.
 
-/* The problem was in test 6, where it has in the first line something like this:
- * 1313 1423 1254 1235
- *
- * I thought that every test would be only one character but no, I need to consider
- * the whole string, maybe to read until a space is found to have all the number and not
- * only one character.
- * */
+/* Each line has some value, that is how many calories the boy will burn if he touch that line.
+ * The second line is how many times he touch that line.*/
 
 #include <iostream>
 #include <string>
@@ -17,6 +13,9 @@ using namespace std;
 
 int main() {
 	int total = 0;
+
+	// Because there are always four lines, we need to read the first line until a ' ' is found,
+	// this is in this way because we want each value. 
 	string str1, str2, str3, str4, str5;
 	getline(cin, str2, ' ');
 	getline(cin, str3, ' ');
@@ -25,6 +24,9 @@ int main() {
 	getline(cin, str1);
 
 	for(size_t i(0); i < str1.size(); i++) {
+		// Because we only have 4 lines, we need to know what line he's touching.
+		// depending on that, we only compute its value, remember that we need to
+		// convert that value to int.
 		if(str1.at(i) == '1') {
 			int aux = stoi(str2);
 			total += aux;
