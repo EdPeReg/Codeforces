ff470cf2993044c1bf74bebd788b0d4cdf36761b
Eduardo Perez Regin <dardo1818@gmail.com>
2020-04-11 22:46:26 -0500

    Final 2

--- 700/Bus to udayland/main.cpp
+++ 700/Bus to udayland/main.cpp
@@ -1,16 +1,26 @@
+// https://codeforces.com/problemset/problem/711/A
+// 1. 28 min
+// 2. 30 min
+
+/* You only need to find a pair of OO, the first pair of OO you find, you will  
+ * change it with ++ 
+ * */
+
+
 #include <iostream>
 
 using namespace std;
 
-// XX|X0
-// 0X|00
 
 int main() {
-	bool flag = false;
+	// This will help you to break the for loop once the first pair OO 
+	// are found.
+	bool flag = false; 
 	int n;
 	char str;
 	cin >> n;
 
+	// Only the i row will change.
 	char matrix[n][5];
 
 	for(int i = 0; i < n; i++) {
@@ -19,8 +29,18 @@ int main() {
 			matrix[i][j] = str;
 		}
 	}
+
 	for(int i = 0; i < n; i++) {
 		for(int j = 0; j < 5; j++) {
+			// To avoid this case:
+			// XX|XO
+			// OX|XX
+			//
+			// XX|X+
+			// +X|XX
+			//
+			// We need to know if the seat that is to the most right is 4, that means, 
+			// do this if the seat of the most right is not the last one. 
 			if(matrix[i][j] == 'O' and matrix[i][j + 1] == 'O' and j != 4) {
 				cout << "YES";
 				flag = true;
@@ -29,6 +49,7 @@ int main() {
 				break;
 			}
 		}
+		// Just break here, we find the pair of OO, don't keep iterating.
 		if(flag == true) break;
 	}
 	cout << "\n";
