173597e364361d06375219d495eaab1ba02ed2fe
Eduardo Perez Regin <dardo1818@gmail.com>
2020-04-13 13:21:11 -0500

    Not completed

---
+++ 700/Add odd or subtract even/main.cpp
@@ -0,0 +1,29 @@
+// https://codeforces.com/problemset/problem/1311/A
+// NOT COMPLETED.
+
+/* I didn't unserstand.*/
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std; 
+
+int main() {
+	int t;
+	cin >> t;
+
+	while(t--) {
+		int total = 0;
+		int a, b;
+		cin >> a >> b;
+
+		if(a & 1 == 0 and b & 1 == 0) {
+			int m = max(a, b);
+
+		}
+
+	}
+
+	return 0;
+}
+
