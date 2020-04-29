b5eabc85f6159961dc32f950304d028108bd5fc4
Eduardo Perez Regin <dardo1818@gmail.com>
2020-04-28 20:39:39 -0500

    Final 2

--- 700/Anton and Digits/main.cpp
+++ 700/Anton and Digits/main.cpp
@@ -1,3 +1,14 @@
+// https://codeforces.com/problemset/problem/734/B
+// Time: 45 min.
+
+/* First you need to have all the possible 256 combinations, to have this
+ * you only need the min of k2, k5, k6. 
+ * Once you have it, you only multiply it by 256, to have the amount of 256 you can have.
+ * Once you had the amount of 256, you need to know the amount of 32 that you can have,
+ * is the same logic, because you need either 2 or 3, you need to have the min of digits,
+ * min(k2,k3), doing this you will have the min amount of 32, you multiply it by 32 and
+ * you will have the amount of 32 to finally add it with the amount of 256 you have. */
+
 #include <iostream>
 
 using namespace std;
@@ -16,6 +27,7 @@ int main() {
 	}
 	total = 256 * min;
 
+	// Once you have the 256, you need to remove the digitis.
 	dig2 -= min;
 	dig5 -= min;
 	dig6 -= min;
