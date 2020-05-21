2e335294964c6f95ded1d6db545c483a04d9e125
Eduardo Perez Regin <dardo1818@gmail.com>
2020-05-21 09:50:02 -0500

    Final 2

--- 800/Watermelon/main.cpp
+++ 800/Watermelon/main.cpp
@@ -1,3 +1,8 @@
+// https://codeforces.com/problemset/problem/4/A
+// Time: 18:32
+//
+// There is a pattern when you divide the watermelon the result should not be odd.
+
 #include <iostream>
 
 using namespace std;
