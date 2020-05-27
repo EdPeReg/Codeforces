aa83bf1cc77f37d7e098a79b8b5d32303d2544f1
EdPeReg <dardo1818@gmail.com>
2020-05-27 12:42:41 -0500

    Final 1

--- 800/Word capitalization/main.cpp
+++ 800/Word capitalization/main.cpp
@@ -1,3 +1,6 @@
+// https://codeforces.com/contest/281/problem/A
+// Time: 15 min
+
 #include <iostream>
 
 using namespace std;
@@ -6,9 +9,11 @@ int main() {
 	string str;
 	cin >> str;
 
+	// Check if the first letter is in between capital letters.
 	if(str[0] >= 'A' and str[0] <= 'Z') {
 		std::cout << str << "\n";
 	} else {
+		// Substract 32 to have the correct ascii value.
 		printf("%c", str[0] - 32);
 		for(int i = 1; i < str.size(); i++) {
 			std::cout << str[i];
