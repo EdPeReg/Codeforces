26df38440df8b2cdd18513c004df75a3795c9f38
EdPeReg <dardo1818@gmail.com>
2020-05-27 17:32:07 -0500

    Second try

--- 800/Bryans photos/main.cpp
+++ 800/Bryans photos/main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main() {
+	bool isBlackWhite = true;
 	int n, m;
 	cin >> n >> m;
 	char pixels[n][m];
@@ -13,7 +14,15 @@ int main() {
 		}
 	}
 
-	if(pixels[0][0] == 'W') {
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < m; j++) {
+			if(pixels[i][j] != 'W' and pixels[i][j] != 'G' and pixels[i][j] != 'B') {
+				isBlackWhite = false;
+			} 
+		}
+	}
+
+	if(isBlackWhite) {
 		cout << "#Black&White\n";
 	} else {
 		cout << "#Color\n";
