f13a168b2f860ce92730c04353ce68d00e03c8fb
EdPeReg <dardo1818@gmail.com>
2020-05-28 19:29:03 -0500

    First try

---
+++ 800/Candies and two sisters/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+
+	while(t--) {
+		int aux = 1, n, distr = 0;
+		cin >> n;
+
+		n--;
+		while(n > aux) {
+			n--;
+			aux++;	
+			distr++;
+		}
+		cout << distr << "\n";
+	}
+
+	return 0;
+}
