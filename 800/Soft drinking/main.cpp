b3094c4d0402c2775c43eb6b955e317b0ff96723
EdPeReg <dardo1818@gmail.com>
2020-05-28 20:23:14 -0500

    Not completed

---
+++ 800/Soft drinking/main.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/problemset/problem/151/A
+// Time: 30 min.
+
+/* I just didn't undestand the problem. */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+
+	return 0;
+}
