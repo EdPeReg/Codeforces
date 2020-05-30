310644e759301417ab6278d73554a32e15d3b657
EdPeReg <dardo1818@gmail.com>
2020-05-30 11:32:16 -0500

    First try

---
+++ 800/File name/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, numChr = 0;
+	string str;
+	cin >> n >> str;
+	
+	for(int i = 0; i < str.size() - 2; i++) {
+		if(str.at(i) == 'x' and str.at(i + 1) == 'x' and str.at(i + 2) == 'x') {
+			numChr++;
+		}
+	}
+
+	cout << numChr << "\n";
+
+	return 0;
+}
