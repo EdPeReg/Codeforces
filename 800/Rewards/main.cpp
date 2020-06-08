38e0927350d2705d6fc44462a03157c4a85d6d9b
EdPeReg <dardo1818@gmail.com>
2020-06-08 13:22:31 -0500

    Not completed

--- 800/Rewards/main.cpp
+++ 800/Rewards/main.cpp
@@ -1,3 +1,8 @@
+// https://codeforces.com/problemset/problem/448/A
+// Time 30m.
+
+
+
 #include <iostream>
 #include <vector>
 #include <numeric>
@@ -8,6 +13,8 @@ int main() {
 	vector<int> cups, medals;
 	int n;
 
+	/* The idea for the next two for loops was to have an individual
+	 * vector for cups and medals, in that way I can do the sum easily.*/
 	for(int i = 0; i < 3; i++) {
 		int cup;
 		cin >> cup;
@@ -24,13 +31,13 @@ int main() {
 	int sumCups = accumulate(cups.begin(), cups.end(), 0);
 	int sumMedals = accumulate(medals.begin(), medals.end(), 0);
 	
+	// I only validate the condition of the how many cups and medals are
+	// permitted in each shelf, but not about the number of shelfs.
 	if(sumCups <= 5 and sumMedals <= 10 and n != 1) {
 		cout << "YES\n";	
 	} else {
 		cout << "NO\n";	
 	}
-
-
 
 	return 0;
 }
