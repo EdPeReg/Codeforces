19d6aec1a2ee605e3c9534871bba2060b4ad8368
EdPeReg <dardo1818@gmail.com>
2020-06-18 11:02:42 -0500

    Not completed

--- 800/Maximum increase/main.cpp
+++ 800/Maximum increase/main.cpp
@@ -16,15 +16,21 @@ int main() {
 	}
 
 	for(size_t i = 0; i < arr.size() - 1; i++) {
+		// This only works when the elements analized are the last, if a subarray
 		if(arr.at(i + 1) > arr.at(i)) {
 			answer.push_back(i);
+			// The idea was also to include the previous element but it doesn't
+			// work properly.
+			//answer.push_back(i + 1);
 		} else {
+			// The problem with this, if the subarray is at any other part besides
+			// the end, that subarray is deleted.
+			// 1 2 3 1 -> the subarray 1 2 3 is deleted.
 			answer.clear();
 		}
 	}
 
 	cout << answer.size() + 1 << "\n";
-
 
 	return 0;
 }
