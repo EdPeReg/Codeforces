3466411eb8cd4bdff9b1ec935378baba916c5900
EdPeReg <dardo1818@gmail.com>
2020-06-23 11:45:07 -0500

    Final 1

--- 800/Hulk/main.cpp
+++ 800/Hulk/main.cpp
@@ -1,3 +1,6 @@
+// https://codeforces.com/problemset/problem/705/A
+// Time: 53 min.
+
 #include <iostream>
 #include <string>
 
@@ -5,6 +8,8 @@ using namespace std;
 
 int main() {
 	string str;
+
+	// 7 because it's the begginig where the word "that" will be inserted.
 	int count = 7;
 	int n;
 	cin >> n;
@@ -20,8 +25,13 @@ int main() {
 				str += "I hate ";
 			}
 
+			// We don't want to insert the word "that" at the end of the 
+			// string.
 			if(i != n) {
 				str.insert(count, "that ");
+				// This will be now the new position to be inserted, it's 12
+				// because we are taking in consideration that previously 
+				// "that" was inserted. 
 				count += 12;
 			} 
 		}
