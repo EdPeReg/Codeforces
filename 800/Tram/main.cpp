41f015040b12a91774815c4bee851d9bd39d83b8
EdPeReg <dardo1818@gmail.com>
2020-08-28 19:24:12 -0500

    Final 2

--- 800/Tram/main.cpp
+++ 800/Tram/main.cpp
@@ -16,11 +16,16 @@ int main() {
 		int out, in;
 		cin >> out >> in;
 		
+		// In the first stop, the maximum number of seats are the people
+		// who enters in the tram.
 		if(firstTime) {
 			total = in;
 			max = total;
 			firstTime = false;
 		} else {
+			// After that first stop, each stop we save how many people were in the tram,
+			// the maximum amount of people that were in the tram, are the maximum amount of
+			// tram's capacity.
 			total = total - out + in;
 			if(total > max) {
 				max = total;
