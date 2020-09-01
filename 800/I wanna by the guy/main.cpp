39c798592513822a67b5e1198ec69a12e93f1be1
EdPeReg <dardo1818@gmail.com>
2020-09-01 12:36:16 -0500

    Not completed 1

--- 800/I wanna by the guy/main.cpp
+++ 800/I wanna by the guy/main.cpp
@@ -1,6 +1,9 @@
 // https://codeforces.com/problemset/problem/469/A
 // Time: 1h 10 min.
 
+/* It seems that works, the problem is to know the amounf of number that has the input. 
+ * Sometimes is not from 0 to level of numbers. */
+
 #include <iostream>
 #include <set>
 #include <algorithm>
@@ -39,33 +42,5 @@ int main() {
 		cout << "Oh, my keyboard!\n";
 	}
 
-	//set<int> levels, kx, ky, yes;
-	//int n;
-	//cin >> n;
-
-	//for(int i = 1; i <= n; i++) {
-		//levels.insert(i);
-	//}
-
-	//for(int i = 0; i < n; i++) {
-		//int num;
-		//cin >> num;
-		//kx.insert(num);
-	//}
-
-	//for(int i = 0; i < n - 1; i++) {
-		//int num;
-		//cin >> num;
-		//ky.insert(num);
-	//}
-
-	 //I was trying to make an union but wrong.
-	//set<int>::iterator it = set_union(kx, kx.begin(), ky, ky.end(), yes.begin());
-
-	 //SEE IF THE ELEMENTS OF THE FINAL SET ARE IN THE LEVEL SET.
-	//for(auto itr = levels.begin(); itr != levels.end(); itr++) {
-		//cout << *itr;
-	//}
-
-	//return 0;
+	return 0;
 }
