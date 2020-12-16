58ccf0ac3821b45cc6c09b08453b68be48b3b17b
EdPeReg <dardo1818@gmail.com>
2020-12-16 11:27:47 -0600

    Not completed 2

--- 800/Again Twenty Five!/main.cpp
+++ 800/Again Twenty Five!/main.cpp
@@ -1,3 +1,9 @@
+// https://codeforces.com/problemset/problem/630/A
+// Time 30 min.
+
+/* Yeah, basically I couldn't figure out how to recieve such big numbers,
+ * but REALLY big. */
+
 #include <iostream>
 #include <cmath>
 #include <sstream>
@@ -5,7 +11,7 @@
 int main() {
 	size_t n;
 	std::cin >> n;
-	double result = pow(5,n);
+	double result = pow(5,n); // Here's the problem, return INF with BUG numbers.
 	result /= 100;
 	
 	std::ostringstream strs;
