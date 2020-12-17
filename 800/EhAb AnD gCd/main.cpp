2f2cce1368e60a8953f18b0fecc914d11d824295
EdPeReg <dardo1818@gmail.com>
2020-12-17 12:06:01 -0600

    Try 1

---
+++ 800/EhAb AnD gCd/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int gcd(int a, int b) {
+	if(b == 0)
+		return a;
+	return gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+	return (a*b) / (gcd(a,b));
+}
+
+int main() {
+	int t;
+	std::cin >> t;
+	while(t--) {
+		int x;
+		bool find = false;
+		std::cin >> x;
+
+		for(int i = 1; i < x; ++i) {
+			for(int j = 1; j < x; ++j) {
+				if(gcd(i,j) + lcm(i,j) == x) {
+					std::cout << i << " " << j << '\n';
+					find = true;
+					break;
+				}
+			}
+			if(find) break;
+		}
+
+	}
+	
+
+	return 0;
+}
