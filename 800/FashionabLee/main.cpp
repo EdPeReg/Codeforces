71388a444c960973fbc45597863d6afcc34a2f59
EdPeReg <dardo1818@gmail.com>
2021-01-01 13:01:10 -0600

    Completed 2

--- 800/FashionabLee/main.cpp
+++ 800/FashionabLee/main.cpp
@@ -1,6 +1,11 @@
 // https://codeforces.com/problemset/problem/1369/A
 // Time: 35 min.
 
+/* 
+ * Just try to draw each figure, you can see a pattern.
+ * Every four-sided figure is beautiful.
+ * */
+
 #include <iostream>
 
 int main() {
