f4a187e46c0ba2b80c74355fa5e3ac51af7b3fa7
EdPeReg <dardo1818@gmail.com>
2021-01-05 14:17:51 -0600

    Not completed 2

--- 800/Array with Odd Sum/main.cpp
+++ 800/Array with Odd Sum/main.cpp
@@ -2,25 +2,42 @@
 // Time: 11 min.
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
+	vector<int> vect;
 	int t, x, n;
 	cin >> t;
 
 	while(t--) {
-		int sum = 0;
 		cin >> x;
 		for(int i = 0; i < x; ++i) {
 			cin >> n;
-			sum += n;
+			vect.push_back(n);
 		}
-		if(sum % 2 == 0) {
-			cout << "YES\n";
-		} else {
+
+		int j = 0;
+		bool find = false;
+		for(int i = 0; i < x; ++i) {
+			for(int j = 1; j < x; ++j) {
+				if((vect.at(i) + vect.at(j)) % 2 == 1) {
+					cout << "YES\n";
+					find = true;
+					break;
+				} else {
+					find = false;
+				}
+			}
+			if(find) {
+				break;
+			}
+		}
+		if(!find) {
 			cout << "NO\n";
 		}
+		vect.clear();
 	}
 
 	return 0;
