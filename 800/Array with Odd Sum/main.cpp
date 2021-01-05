4b30a5ff49776df6ad3f050820df948dbcb2297f
EdPeReg <dardo1818@gmail.com>
2021-01-05 13:05:46 -0600

    Try 1

---
+++ 800/Array with Odd Sum/main.cpp
@@ -0,0 +1,27 @@
+// https://codeforces.com/problemset/problem/1296/A
+// Time: 11 min.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t, x, n;
+	cin >> t;
+
+	while(t--) {
+		int sum = 0;
+		cin >> x;
+		for(int i = 0; i < x; ++i) {
+			cin >> n;
+			sum += n;
+		}
+		if(sum % 2) {
+			cout << "YES\n";
+		} else {
+			cout << "NO\n";
+		}
+	}
+
+	return 0;
+}
