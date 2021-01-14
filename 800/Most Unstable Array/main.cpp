9055a38d425ea83a81c53831cc03df2a2b1102eb
EdPeReg <dardo1818@gmail.com>
2021-01-14 13:32:58 -0600

    Completed 1

--- 800/Most Unstable Array/main.cpp
+++ 800/Most Unstable Array/main.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-	long long t,n,m;
+	int t,n,m;
 	cin >> t;
 
 	while(t--) {
 		cin >> n >> m;
-		if(n == 1) {
-			cout << 0 << '\n';
-		} else if(n == 2) {
-			cout << m << '\n';
-		} else {
-			cout << m*2 << '\n';
-		}
+		cout << min(2,n-1)*m << '\n';
 	}
 
 	return 0;
