4b6c95c605cc3499d5aad51fe84a97fed83470f2
EdPeReg <dardo1818@gmail.com>
2021-01-17 15:07:34 -0600

    Try 1

--- 800/Honest Coach/main.cpp
+++ 800/Honest Coach/main.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <cmath>
+#include <algorithm>
 
 int main() {
 	int t, n;
-	std::cin >> t >> n;
+	std::cin >> t;
 
 	while(t--) {
+		std::cin >> n;
 		int arr[n];
 		for(int i = 0; i < n; ++i) {
 			std::cin >> arr[i];
 		}
+		std::sort(arr,arr+n);
 
-		if(n % 2) {
-			std::cout << abs(arr[n/2] - arr[n/2 + 1]);
+		if(n == 2) {
+			std::cout << abs(arr[0] - arr[1]) << '\n';
+		} else if(n % 2) {
+			std::cout << abs(arr[n/2] - arr[n/2 + 1]) << '\n';
 		} else {
-			std::cout << abs(arr[(n-1)/2] - arr[(n-1)/2 - 1]);
+			std::cout << abs(arr[(n-1)/2] - arr[(n-1)/2 - 1]) << '\n';
 		}
 	}
 
