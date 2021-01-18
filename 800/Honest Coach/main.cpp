2ffc4a51897da59228916d29925a93430698bb0a
EdPeReg <dardo1818@gmail.com>
2021-01-17 18:36:49 -0600

    Completed 2

--- 800/Honest Coach/main.cpp
+++ 800/Honest Coach/main.cpp
@@ -1,5 +1,7 @@
+// https://codeforces.com/contest/1360/problem/B
+// Time 1h.
+
 #include <iostream>
-#include <cmath>
 #include <algorithm>
 
 int main() {
@@ -13,14 +15,19 @@ int main() {
 		for(int i = 0; i < n; ++i) {
 			std::cin >> arr[i];
 		}
+		// Is necessary to sort our array to get the minimum difference between the numbers.
 		std::sort(arr,arr+n);
 	
 		int j = 0;
 		for(int i = n-1; i > 0; --i) {
+			// Store the difference that exist between numbers, starting from out last element of our array.
+			// To avoid negative numbers.
 			diff[j++] = arr[i] - arr[i-1]; 
 		}
 
+		// n-1 to not exceed your array limit.
 		std::sort(diff,diff+(n-1));
+		// The minimum difference will be our answer.
 		std::cout << diff[0] << '\n';
 	}
 
