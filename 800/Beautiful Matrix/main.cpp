46d6acf5b7d445b661a6d85676222102789e7cf4
EdPeReg <dardo1818@gmail.com>
2021-01-18 11:54:17 -0600

    Improved 1

--- 800/Beautiful Matrix/main.cpp
+++ 800/Beautiful Matrix/main.cpp
@@ -7,28 +7,15 @@
 int main() {
 	int m[5][5];
 
-	for(int i = 0; i < 5; ++i) {
-		for(int j = 0; j < 5; ++j) {
-			std::cin >> m[i][j];
-		}
-	}
-
-	int r,c;
-	bool stop = false;
-	for(int i = 0; i < 5; ++i) {
-		for(int j = 0; j < 5; ++j) {
-			if(m[i][j] == 1) {
-				r = i;
-				c = j;
-				stop = true;
-				break;
+	int n;
+	for(int i = 1; i <= 5; ++i) {
+		for(int j = 1; j <= 5; ++j) {
+			std::cin >> n;
+			if(n == 1) {
+				std::cout << abs(3-i) + abs(3-j) << '\n'; 
 			}
 		}
-		if(stop) break;
 	}
-
-	// [3][3] Taking in consideration that starts with 1.
-	std::cout << abs(3-r) + abs(3-c) << '\n'; 
 
 	return 0;
 }
