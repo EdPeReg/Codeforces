f819d58f1b251c71f97425fa126a11e4a29d9dac
EdPeReg <dardo1818@gmail.com>
2021-01-18 11:44:49 -0600

    Improved 1

--- 800/Beautiful Matrix/main.cpp
+++ 800/Beautiful Matrix/main.cpp
@@ -2,6 +2,7 @@
 // 	Time: 30m.
 
 #include <iostream>
+#include <cmath>
 
 int main() {
 	int m[5][5];
@@ -12,13 +13,13 @@ int main() {
 		}
 	}
 
-	int i1,j1;
+	int r,c;
 	bool stop = false;
 	for(int i = 0; i < 5; ++i) {
 		for(int j = 0; j < 5; ++j) {
 			if(m[i][j] == 1) {
-				i1 = i;
-				j1 = j;
+				r = i;
+				c = j;
 				stop = true;
 				break;
 			}
@@ -26,25 +27,8 @@ int main() {
 		if(stop) break;
 	}
 
-	int moves = 0;
-	// [2][2] is our middle.
-	while(i1 != 2 or j1 != 2) {
-		if(i1 > 2) {
-			--i1;
-			++moves;
-		} else if(i1 < 2){
-			++i1;
-			++moves;
-		}
+	// [3][3] Taking in consideration that starts with 1.
+	std::cout << abs(3-r) + abs(3-c) << '\n'; 
 
-		if(j1 > 2) {
-			--j1;
-			++moves;
-		} else if(j1 < 2){
-			++j1;
-			++moves;
-		}
-	}
-	std::cout << moves << '\n';
 	return 0;
 }
