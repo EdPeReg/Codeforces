0d0ae8ff022f28123b8c468e40a6b2365513e19d
EdPeReg <dardo1818@gmail.com>
2021-01-17 18:24:57 -0600

    Try 2

--- 800/Honest Coach/main.cpp
+++ 800/Honest Coach/main.cpp
@@ -9,18 +9,19 @@ int main() {
 	while(t--) {
 		std::cin >> n;
 		int arr[n];
+		int diff[n];
 		for(int i = 0; i < n; ++i) {
 			std::cin >> arr[i];
 		}
 		std::sort(arr,arr+n);
-
-		if(n == 2) {
-			std::cout << abs(arr[0] - arr[1]) << '\n';
-		} else if(n % 2) {
-			std::cout << abs(arr[n/2] - arr[n/2 + 1]) << '\n';
-		} else {
-			std::cout << abs(arr[(n-1)/2] - arr[(n-1)/2 - 1]) << '\n';
+	
+		int j = 0;
+		for(int i = n-1; i > 0; --i) {
+			diff[j++] = arr[i] - arr[i-1]; 
 		}
+
+		std::sort(diff,diff+(n-1));
+		std::cout << diff[0] << '\n';
 	}
 
 	return 0;
