917d304d5af44e7d22da7e7187987f936b91418c
EdPeReg <dardo1818@gmail.com>
2021-01-22 13:06:57 -0600

    Try 1

---
+++ 800/Gifts Fixing/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+
+	while(t--) {
+		int n;
+		size_t sum = 0;
+		cin >> n;
+		int c[n], o[n];
+
+		for(int i = 0; i < n; ++i) {
+			cin >> c[i];
+		}
+
+		for(int i = 0; i < n; ++i) {
+			cin >> o[i];
+		}
+
+		for(int i = 0; i < n; ++i) {
+			sum += max(c[i] - *min_element(c,c+n), o[i] - *min_element(o,o+n));
+		}
+		cout << sum << '\n';
+	}
+}
