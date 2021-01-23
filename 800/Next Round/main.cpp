6e7f0b3decdeac5bed65579d1f295a824f007748
EdPeReg <dardo1818@gmail.com>
2021-01-23 10:24:22 -0600

    Try 2

---
+++ 800/Next Round/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int k, n, sum = 0;
+	cin >> n >> k;
+
+	int arr[n];
+	for(int& e : arr) cin >> e;
+
+	for(int i = 0; i < n; ++i) {
+		if(arr[i] >= arr[k] and arr[i] > 0) ++sum;
+	}
+	cout << sum << '\n';
+
+	return 0;
+}
