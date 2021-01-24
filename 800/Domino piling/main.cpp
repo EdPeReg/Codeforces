ae0701eeffd97170bb6963a28a6b5534cc4d64e9
EdPeReg <dardo1818@gmail.com>
2021-01-24 12:25:25 -0600

    Try 1

---
+++ 800/Domino piling/main.cpp
@@ -0,0 +1,14 @@
+// https://codeforces.com/problemset/problem/50/A
+// Time: 5 min.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int m, n;
+	cin >> m >> n;
+
+	cout << (m*n)/2 << '\n';
+
+}
