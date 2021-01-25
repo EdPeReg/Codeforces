13ba8773dbbef9c0d469f6d50a8b94171dd8d858
EdPeReg <dardo1818@gmail.com>
2021-01-25 12:19:01 -0600

    Try 1

---
+++ 800/Bit++/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	int n, x = 0;
+	std::string str;
+	std::cin >> n;
+
+	while(n--) {
+		std::cin >> str;
+		if(str.find("+") != std::string::npos) {
+			++x;
+		} else {
+			--x;
+		}
+	}
+	std::cout << x << '\n';
+}
+
