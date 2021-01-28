77587edc57abd16df6c05822244e12e40f2b5a03
EdPeReg <dardo1818@gmail.com>
2021-01-28 13:02:13 -0600

    Try 1

---
+++ 800/Word/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cctype>
+
+int main() {
+	std::string str;
+	std::cin >> str;
+
+	int lower = 0, upper = 0;
+	for(auto& c : str) {
+		if(isupper(c)) ++upper;
+		else ++lower;
+	}
+
+	if(upper == lower) {
+		for(auto& c : str) {
+			if(!islower(c)) c += 32;
+		}
+	} else if(upper < lower) {
+		for(auto& c : str) {
+			if(!islower(c)) c += 32;
+		}
+	} else {
+		for(auto& c : str) {
+			if(!isupper(c)) c += 32;
+		}
+	}
+	std::cout << str << '\n';
+
+	return 0;
+}
