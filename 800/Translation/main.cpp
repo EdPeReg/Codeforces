ad8fa8ea4a5688a6e292f4274f97f0abaa8761ce
EdPeReg <dardo1818@gmail.com>
2021-09-18 14:10:30 -0500

    2 Improved, built-in function used

--- 800/Translation/main.cpp
+++ 800/Translation/main.cpp
@@ -1,20 +1,14 @@
 // 8:03
 
 #include <iostream>
+#include <algorithm>
 
 int main()
 {
     std::string s, t;
     std::cin >> s >> t;
     
-    int j = t.size() - 1;
-    for(int i = 0; i < s.size(); ++i) {
-        if(s[i] != t[j]) {
-            std::cout << "NO";
-            break;
-        }
-        --j;
-    }
-
-    if(j < 0) std::cout << "YES";
+    std::reverse(s.begin(), s.end());
+    if(s == t) std::cout << "YES";
+    else std::cout << "NO";
 }
