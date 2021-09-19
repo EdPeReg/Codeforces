33b39776293f707dd83be54c89b3299c2ccc3504
EdPeReg <dardo1818@gmail.com>
2021-09-18 19:27:09 -0500

    2 - Spy detected!

--- 800/Spy Detected!/main.cpp
+++ 800/Spy Detected!/main.cpp
@@ -1,7 +1,8 @@
-// 10:05
+// 20:15
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 int main() {
     int t, n, a;
@@ -10,22 +11,26 @@ int main() {
     while(t--) {
         std::cin >> n;
         
-        int index = 1;
         std::vector<int> v(n);
         for(int i = 0; i < n; ++i) {
             std::cin >> v[i]; 
         }
-
-        for(int i = 1; i < n; ++i) {
-            if(v[i] != v[i-1]) {
+        
+        int index = 0;
+        for(int i = 0; i < n - 1; ++i) {
+            if(v[i] != v[i+1]) {
                 index = i;
+                break;
             }
         }
 
-        if(v[v.size() - 1] != v[v.size() - 2])
-            std::cout << ++index << '\n';
-        else 
-            std::cout << index << '\n';
+        // We have multiple numbers that aren't intruders.
+        if(std::count(v.begin(), v.end(), v[index]) > 1) {
+            std::cout << index + 2 << '\n';
+        } 
+        // When the is the first one, only add 1.
+        else {
+            std::cout << index + 1 << '\n';
+        }
     }
 }
-
